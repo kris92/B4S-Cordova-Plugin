1a66d5699f0e372863f59e00d55af75ff4b4b827
Christophe Janot <kris92@me.com>
2016-11-07 10:27:44 +0100

    Fix android gradlefile name

---
+++ src/android/b4s.gradle
@@ -0,0 +1,12 @@
+dependencies {
+    // B4S dependencies
+    compile 'nl.qbusict:cupboard:2.1.4'
+    compile 'de.greenrobot:eventbus:2.4.0'
+    compile 'com.squareup.retrofit2:converter-jackson:2.0.0'
+    compile 'com.google.android.gms:play-services-location:9.2.1'
+    compile 'com.google.android.gms:play-services-ads:9.2.1'
+    compile 'com.google.android.gms:play-services-gcm:9.2.1'
+
+    compile (name:'b4s-android-sdk', ext:'aar')
+    compile (name:'b4s-android-sdk-playservices921', ext:'aar')
+}
